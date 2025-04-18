c56d8ece3b22bf0a6eabe5366c14a3bda2a9096a
suraj-mali-1423 <surajsmali@coep.sveri.ac.in>
2025-04-18 08:00:57 +0530

    Class And Object

--- Class.java
+++ Class.java
@@ -1,5 +1,17 @@
+class Pen{ // class 
+    String Color ;
+    String Brand_Name ;
+    public void Display(){
+       System.out.println("Successfully Written!");
+    }
+}
+
 class Class {
    public static void main(String[] args) {
-     System.out.println("jbfjb"); 
+     Pen P1 = new Pen(); // P1 is the object of the Pen class which is used to access the entity .
+     P1.Display();
+     
+     
+
    }
 }
